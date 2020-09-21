fa01994e20d5fbb01b5f6a4938cd32bcf6ed23c7
skrall3119 <alexjanci@yahoo.com>
2020-09-21 14:16:09 -0500

    Initial Commit

---
+++ Family Tree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Family Tree
+//
+//  Created by Alex Janci on 9/21/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
